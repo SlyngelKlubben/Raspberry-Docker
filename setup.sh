5396e69baba67e53f9722614a6c4318e8c7edba4
Peter <55195580+pete662f@users.noreply.github.com>
2021-03-16 17:24:14 +0100

    Update setup.sh
    Fixed copy folders

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ git clone https://github.com/SlyngelKlubben/Raspberry-Docker.git
 cd ./Raspberry-Docker
 curl -fsSL https://raw.githubusercontent.com/SensorsIot/IOTstack/master/install.sh | bash
 cp -f ./docker-compose.yml ./IOTstack
-cp ./services ./IOTstack
-cp ./volumes ./IOTstack
+cp -r./services ./IOTstack
+cp -r ./volumes ./IOTstack
 git clone https://github.com/prest/prest.git ./IOTstack/services/prest
 cp -f ./Dockerfile ./IOTstack/services/prest
 echo "Reboot to make this work properly"
