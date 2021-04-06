ad80c88f4896936136d83e94255f7f845ac3f730
Peter <55195580+pete662f@users.noreply.github.com>
2021-04-06 16:40:42 +0200

    Added fast way to install docker-compose.yml

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get install git -y
 git clone https://github.com/SlyngelKlubben/Raspberry-Docker.git
 cd ./Raspberry-Docker
+curl -fsSL https://raw.githubusercontent.com/SlyngelKlubben/Raspberry-Docker/master/docker-compose.yml
 curl -fsSL https://raw.githubusercontent.com/SensorsIot/IOTstack/master/install.sh | bash
 find_ip=`ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
 sed -i "s/PREST_PG_HOST=/PREST_PG_HOST=$find_ip/g" docker-compose.yml
