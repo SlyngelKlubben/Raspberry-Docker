455f1b76dc75c16567b5ebbd0356ab2adc4bf252
Peter <55195580+pete662f@users.noreply.github.com>
2021-04-06 16:34:50 +0200

    Fixed finding ip

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ sudo apt-get install git -y
 git clone https://github.com/SlyngelKlubben/Raspberry-Docker.git
 cd ./Raspberry-Docker
 curl -fsSL https://raw.githubusercontent.com/SensorsIot/IOTstack/master/install.sh | bash
-lip=`hostname -I`
-sed -i "s/PREST_PG_HOST=/PREST_PG_HOST=$lip/g" docker-compose.yml
+find_ip=`ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+sed -i "s/PREST_PG_HOST=/PREST_PG_HOST=$find_ip/g" docker-compose.yml
 cp -f ./docker-compose.yml ./IOTstack
 cp -r ./services ./IOTstack
 cp -r ./volumes ./IOTstack
