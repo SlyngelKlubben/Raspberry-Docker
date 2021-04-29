7b3f3314e2ec76d6fc318680b4ade17b5b8f8c49
Peter <55195580+pete662f@users.noreply.github.com>
2021-04-29 22:38:21 +0200

    Removed test setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-#sudo apt-get install git -y
-#git clone https://github.com/SlyngelKlubben/Raspberry-Docker.git
-#cd ./Raspberry-Docker
-#curl -fsSL https://raw.githubusercontent.com/SlyngelKlubben/Raspberry-Docker/master/docker-compose.yml | cat > docker-compose.yml
-#curl -fsSL https://raw.githubusercontent.com/SensorsIot/IOTstack/master/install.sh | bash
+sudo apt-get install git -y
+git clone https://github.com/SlyngelKlubben/Raspberry-Docker.git
+cd ./Raspberry-Docker
+curl -fsSL https://raw.githubusercontent.com/SlyngelKlubben/Raspberry-Docker/master/docker-compose.yml | cat > docker-compose.yml
+curl -fsSL https://raw.githubusercontent.com/SensorsIot/IOTstack/master/install.sh | bash
 
 yml_file="docker-compose.yml"
 
@@ -20,9 +20,9 @@ sed -i "s/{pg_user}/$pg_user/g" $yml_file
 sed -i "s/{pg_pass}/$pg_pass/g" $yml_file
 sed -i "s/{pg_db}/$pg_db/g" $yml_file
 
-#cp -f ./docker-compose.yml ./IOTstack
-#cp -r ./services ./IOTstack
-#cp -r ./volumes ./IOTstack
-#git clone https://github.com/prest/prest.git ./IOTstack/services/prest
-#cp -f ./Dockerfile ./IOTstack/services/prest
+cp -f ./docker-compose.yml ./IOTstack
+cp -r ./services ./IOTstack
+cp -r ./volumes ./IOTstack
+git clone https://github.com/prest/prest.git ./IOTstack/services/prest
+cp -f ./Dockerfile ./IOTstack/services/prest
 echo "Reboot to make this work properly"
